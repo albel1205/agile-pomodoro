0dffdb8815ae16371d5ce3cbea8ffecc18792309
Kien Tran <kientnt90@gmail.com>
2015-10-25 12:59:18 +0700

    update the inventory page template

--- modules/inventory/inventory.ctrl.js
+++ modules/inventory/inventory.ctrl.js
@@ -1,5 +1,9 @@
 function InventoryController($scope, pageService){
 	$scope.isCurrentPage = function(){
 		return pageService.getCurrentPageId() == 2;
-	}
+	};
+	
+	$scope.goToToDoPage = function(){
+		pageService.setCurrentPageId(3);
+	};
 }
